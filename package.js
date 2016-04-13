7bf1b559a2552f52faae94ab41f5f034d873bfcd
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-04-13 11:35:26 +0300

    Remove comments

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'apinf:restivus-swagger',
   version: '0.2.1',
-  // Brief, one-line summary of the package.
   summary: 'Generate Swagger doc for API',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/apinf/restivus-swagger',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
