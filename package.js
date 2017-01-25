066ae1ca294c4e0e0ab2c456d184a86827467754
Alfred J. Kwak <alfredjkwak@protonmail.com>
2017-01-25 23:45:01 +0200

    update package name, links & dependencies

--- package.js
+++ package.js
@@ -1,24 +1,24 @@
 Package.describe({
-  name: 'apinf:restivus-swagger',
-  version: '0.2.1',
+  name: 'ajkwak:restivus-swagger',
+  version: '0.2.3',
   summary: 'Generate Swagger doc for API',
-  git: 'https://github.com/apinf/restivus-swagger',
+  git: 'https://github.com/alfredjkwak/restivus-swagger',
   documentation: 'README.md'
 });
 
 Npm.depends({
-  "url-parse": "1.0.5"
+  "url-parse": "1.1.7"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use(['ecmascript','underscore','nimble:restivus@0.8.7']);
+  api.use(['ecmascript','underscore','nimble:restivus@0.8.12']);
   api.addFiles('server/restivus-swagger.js', ['server']);
 });
 
 Package.onTest(function(api) {
-  api.use(['ecmascript','underscore','nimble:restivus@0.8.7']);
-  api.use('apinf:restivus-swagger');
+  api.use(['ecmascript','underscore','nimble:restivus@0.8.12']);
+  api.use('ajkwak:restivus-swagger');
   api.use('tinytest');
   api.addFiles('test.js');
 });
