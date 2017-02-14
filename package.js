2305315e3cbee9ccd96467341d03bc4c53d258fa
Alfred J. Kwak <alfredjkwak@protonmail.com>
2017-02-14 05:06:18 +0200

    update package names for organization

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'ajkwak:restivus-swagger',
+  name: 'mrest:restivus-swagger',
   version: '0.2.5',
   summary: 'Generate Swagger doc for API',
-  git: 'https://github.com/alfredjkwak/restivus-swagger',
+  git: 'https://github.com/meteor-rest/restivus-swagger',
   documentation: 'README.md'
 });
 
@@ -12,13 +12,13 @@ Npm.depends({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use(['ecmascript','underscore','ajkwak:restivus']);
+  api.use(['ecmascript','underscore','mrest:restivus']);
   api.addFiles('server/restivus-swagger.js', ['server']);
 });
 
 Package.onTest(function(api) {
-  api.use(['ecmascript','underscore','ajkwak:restivus']);
-  api.use('ajkwak:restivus-swagger');
+  api.use(['ecmascript','underscore','mrest:restivus']);
+  api.use('mrest:restivus-swagger');
   api.use('tinytest');
   api.addFiles('test.js');
 });
